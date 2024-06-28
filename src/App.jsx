11f279dee7bb21c831643893727f223c21ab8e0d
Henrique Lengruber <henriquerl22@outlook.com>
2024-06-28 15:48:12 -0300

    feat: create and update Document/File Uploads

--- src/App.jsx
+++ src/App.jsx
@@ -2,53 +2,129 @@ import React from "react";
 import { useState, useEffect } from "react";
 import "./App.css";
 import { Auth } from "./components/auth";
-import { db } from "./config/firebase";
-import { getDocs, collection } from "firebase/firestore";
+import { db, auth, storage } from "./config/firebase";
+import { getDocs, collection, addDoc, deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { ref, uploadBytes } from "firebase/storage";
 
 function App() {
   const [documentList, setDocumentList] = useState([]);
 
+  //Criação de um novo documento
+  const [newDocumentTitle, setNewDocumentTitle] = useState("");
+  const [newDocumentStatus, setNewDocumentStatus] = useState("");
+  const [newDocumentReceiver, setNewDocumentReceiver] = useState("");
+
+  //Atualiza um documento
+  const [updateDocumentTitle, setUpdateDocumentTitle] = useState("");
+
+  // File upload State
+  const [fileUpload, setFileUpload] = useState(null);
+
   const documentsColletionRef = collection(db, "documentList");
 
+  const getDocumentList = async () => {
+    try {
+      const data = await getDocs(documentsColletionRef);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setDocumentList(filteredData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
-    const getDocumentList = async () => {
-      try {
-        const data = await getDocs(documentsColletionRef);
-        const filteredData = data.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-        setDocumentList(filteredData);
-      } catch (error) {
-        console.error(error);
-      }
-    };
     getDocumentList();
   }, []);
+
+  const onSubmitDocument = async () => {
+    try {
+      await addDoc(documentsColletionRef, {
+        nome: newDocumentTitle,
+        status: newDocumentStatus,
+        destinatario: newDocumentReceiver,
+        userId: auth?.currentUser.uid,
+      });
+      getDocumentList();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deleteDocument = async (id) => {
+    try {
+      const documentDoc = doc(db, "documentList", id);
+      await deleteDoc(documentDoc);
+      getDocumentList();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const updatedDocumentTitle = async (id) => {
+    try {
+      const documentDoc = doc(db, "documentList", id);
+      await updateDoc(documentDoc, {
+        nome: updateDocumentTitle
+      });
+      getDocumentList();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const uploadFile = async () => {
+    if (fileUpload == null) return;
+    const fileFolderRef = ref(storage, `projectFiles/${fileUpload.name}`);
+    try {
+      await uploadBytes(fileFolderRef, fileUpload);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
       <div>
         <Auth />
 
-<div>
+        <div>
+          <input
+            placeholder="Titulo"
+            onChange={(e) => setNewDocumentTitle(e.target.value)}
+          />
+          <input
+            placeholder="Destinatário"
+            onChange={(e) => setNewDocumentReceiver(e.target.value)}
+          />
+          <input
+            placeholder="Status"
+            onChange={(e) => setNewDocumentStatus(e.target.value)}
+          />
 
-<input placeholder="Nome"/>
-<input placeholder="Destinatário" type="autocomplete"/>
-<input placeholder="Status"/>
-
-<button>Adicionar</button>
-
-</div>
+          <button onClick={onSubmitDocument}>Adicionar</button>
+        </div>
 
         <div>
           {documentList.map((document) => (
             <div>
               <h1>{document.nome}</h1>
               <p>{document.status}</p>
+              <button onClick={() => deleteDocument(document.id)}>Deletar</button>
+              <input placeholder="Novo Titulo" onChange={(e) => setUpdateDocumentTitle(e.target.value)}/>
+              <button onClick={() => updatedDocumentTitle(document.id)}>Atualizar Título</button>
             </div>
           ))}
+        </div>
+
+
+          <div> 
+            <input type="file" onChange={(e) => setFileUpload(e.target.files[0])}/>
+            <button onClick={uploadFile}>Adicionar Documento</button>
           </div>
+
       </div>
     </>
   );
